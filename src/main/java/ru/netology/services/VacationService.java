fecd174893767dbf8768dadf994b6a798214411b
Andrey <justupor@gmail.com>
2024-02-19 21:17:19 +0300

    All tests are stable

---
+++ src/main/java/ru/netology/services/VacationService.java
@@ -0,0 +1,21 @@
+package ru.netology.services;
+public class VacationService {
+
+    public int calculateVacationMonths(int income, int expenses, int threshold) {
+        int vacationMonths = 0;
+        int savings = 0;
+
+        for (int month = 1; month <= 12; month++) {
+            if (savings >= threshold) {
+                vacationMonths++;
+                savings = savings - expenses;
+                savings = savings / 3;
+            } else {
+                savings += income - expenses;
+            }
+        }
+
+        return vacationMonths;
+    }
+}
+
